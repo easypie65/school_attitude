19c2e972105d467a1828d5365226514e6ab951f9
easypie65 <easypie65@gmail.com>
2025-09-01 21:55:42 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 사용자 페이지면 base: '/' 로 두세요.
-// 일반(프로젝트) 레포면 반드시 '/레포이름/' 로!
 export default defineConfig({
   plugins: [react()],
-  base: '/school_attitude/'
+  base: '/school_attitude/'  // ← 레포 이름 그대로!
 })
